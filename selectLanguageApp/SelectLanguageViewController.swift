8338931376454406d8c9b9314542f0319b810385
Takashi Nomura <>
2024-04-19 00:39:37 +0900

    first

--- selectLanguageApp/SelectLanguageViewController.swift
+++ selectLanguageApp/SelectLanguageViewController.swift
@@ -15,45 +15,41 @@ import LanguageManager_iOS
 class SelectLanguageViewController: UIViewController {
     var viewModel: LanguageSelectionViewModel!
 
-      @IBOutlet weak var japaneseButton: UIButton!
-      @IBOutlet weak var englishButton: UIButton!
+    @IBOutlet weak var japaneseButton: UIButton!
+    @IBOutlet weak var englishButton: UIButton!
 
     private let disposeBag = DisposeBag()
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            bindUI()
-        }
-        
-        func bindUI() {
-            japaneseButton.rx.tap
-                .map { "ja" }
-                .bind(to: viewModel.selectLanguage)
-                .disposed(by: disposeBag)
-            englishButton.rx.tap
-                .map { "en" }
-                .bind(to: viewModel.selectLanguage)
-                .disposed(by: disposeBag)
-            viewModel.shouldDismiss
-                .subscribe(onNext: { [weak self] _ in
-                    self?.dismiss(animated: true, completion: nil)
-                })
-                .disposed(by: disposeBag)
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindUI()
     }
+    
+    func bindUI() {
+        japaneseButton.rx.tap
+            .map { "ja" }
+            .bind(to: viewModel.selectLanguage)
+            .disposed(by: disposeBag)
+        englishButton.rx.tap
+            .map { "en" }
+            .bind(to: viewModel.selectLanguage)
+            .disposed(by: disposeBag)
+        viewModel.shouldDismiss
+            .subscribe(onNext: { [weak self] _ in
+                self?.dismiss(animated: true, completion: nil)
+            })
+            .disposed(by: disposeBag)
+    }
+}
 
 class LanguageSelectionViewModel {
 
-    // 言語選択イベントを発行するためのSubject
     let selectLanguage: PublishSubject<String> = PublishSubject()
-
-    // UIを閉じるべきかどうかを伝えるためのSubject
     let shouldDismiss: Observable<Void>
  
     private let disposeBag = DisposeBag()
 
     init() {
-        // 言語選択を購読して、言語設定を更新する
         shouldDismiss = selectLanguage
             .do(onNext: { languageCode in
                 
@@ -62,9 +58,6 @@ class LanguageSelectionViewModel {
             })
             .map { _ in Void() }
             .share()
-        
-
-
     }
 }
 
--- selectLanguageApp/ViewController.swift
+++ selectLanguageApp/ViewController.swift
@@ -15,40 +15,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var showLanguageButton: UIButton!
     private let disposeBag = DisposeBag()
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            setupBindings()
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBindings()
+    }
+    
+    private func setupBindings() {
+        updateTexts()
         
-        private func setupBindings() {
-            // 初期のテキスト設定
-            updateTexts()
-            
-            // ボタンを押下してシートを表示する
-            showLanguageButton.rx.tap
+        showLanguageButton.rx.tap
+            .bind { [weak self] in
+                self?.presentLanguageSheet()
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func presentLanguageSheet() {
+        let storyboard = UIStoryboard(name: "Sheet", bundle: nil)
+        if let languageSelectorVC = storyboard.instantiateViewController(withIdentifier: "Sheet") as? SelectLanguageViewController {
+            let viewModel = LanguageSelectionViewModel()
+            languageSelectorVC.viewModel = viewModel
+            viewModel.shouldDismiss
                 .bind { [weak self] in
-                    self?.presentLanguageSheet()
+                    self?.updateTexts()
                 }
                 .disposed(by: disposeBag)
-        }
-        
-        private func presentLanguageSheet() {
-            let storyboard = UIStoryboard(name: "Sheet", bundle: nil)
-            if let languageSelectorVC = storyboard.instantiateViewController(withIdentifier: "Sheet") as? SelectLanguageViewController {
-                let viewModel = LanguageSelectionViewModel()
-                languageSelectorVC.viewModel = viewModel
-                viewModel.shouldDismiss
-                    .bind { [weak self] in
-                        self?.updateTexts()
-                    }
-                    .disposed(by: disposeBag)
 
-                languageSelectorVC.modalPresentationStyle = .formSheet
-                self.present(languageSelectorVC, animated: true, completion: nil)
-            }
-        }
-
-        private func updateTexts() {
-            titleLabel.text = "select_language".localiz()
+            languageSelectorVC.modalPresentationStyle = .formSheet
+            self.present(languageSelectorVC, animated: true, completion: nil)
         }
     }
+
+    private func updateTexts() {
+        titleLabel.text = "select_language".localiz()
+    }
+}
